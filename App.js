c22f0da464bac366ccd931a5d896252d5708618e
Raúl Montón <raul_m_p@me.com>
2020-07-19 05:45:47 +0200

    FIREBASE IS WORKING (commit 2).

--- App.js
+++ App.js
@@ -17,6 +17,8 @@ import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import auth from '@react-native-firebase/auth'; 
+
 // First view controlled by the tab bar / segmented control
 const FirstRoute = () => (
   <React.Fragment>
@@ -115,6 +117,31 @@ function HomeScreen() {
 
 // Main app
 const App: () => React$Node = () => {
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
