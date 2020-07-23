8ee91e5fe896fe2b3907268d79d335c730f2f45b
Raúl Montón <raul_m_p@me.com>
2020-07-23 13:52:04 +0200

    Implemented basic flatlist.

--- src/screens/HomeScreen/FirstTab.js
+++ src/screens/HomeScreen/FirstTab.js
@@ -15,37 +15,84 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import DraggableFlatList from "react-native-draggable-flatlist";
 
 // This function is called whenever you tap the floating action button
 const TappedFAB = () => (
   alert('Not yet implemented!')
 );
 
+// Data for DraggableFlatList
+
+const exampleData = [...Array(20)].map((d, index) => ({
+  key: `item-${index}`, // For example only -- don't use index as your key!
+  label: index,
+  backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index *
+    5}, ${132})`
+}));
+
+
 // First view controlled by the tab bar / segmented control
-const FirstTab = () => (
+function FirstTab(){
 
-  <React.Fragment>
+  const [listData, setListData] = useState(exampleData);
 
-    <Text style={styles.text}>This is tab A</Text>
+  renderItem = ({ item, index, drag, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? "blue" : item.backgroundColor,
+          alignItems: "center",
+          justifyContent: "center",
+          borderTopLeftRadius: (index == 0) ? 30 : 0, //Rounded corners for the first item
+          borderTopRightRadius: (index == 0) ? 30 : 0 //Rounded corners for the first item
+        }}
+        onLongPress={drag}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 32
+          }}
+        >
+          {item.label}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
 
-    {
-      // Enable floating android button only on Android
-      Platform.OS === 'android' ?
-        <View style={styles.FloatingButtonStyle}>
-          <TouchableOpacity 
-            activeOpacity={0.5} 
-            onPress={TappedFAB} 
-            style={styles.TouchableOpacityStyle} >
-              <Image 
-                source={{uri : 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png'}} 
-                style={styles.FloatingButtonStyle} />
-          </TouchableOpacity>
-        </View> :
-        null
-    }
+  return(
+    <React.Fragment>
 
-  </React.Fragment>
-);
+      <DraggableFlatList
+        data={listData}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `draggable-item-${item.key}`}
+        onDragEnd={({ data }) => setListData({listData})}
+        style={{padding: 20}}
+      />
+
+      {
+        // Enable floating android button only on Android
+        Platform.OS === 'android' ?
+          <View style={styles.FloatingButtonStyle}>
+            <TouchableOpacity 
+              activeOpacity={0.5} 
+              onPress={TappedFAB} 
+              style={styles.TouchableOpacityStyle} >
+                <Image 
+                  source={{uri : 'https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png'}} 
+                  style={styles.FloatingButtonStyle} />
+            </TouchableOpacity>
+          </View> :
+          null
+      }
+
+    </React.Fragment>
+  );
+}
 
 const styles = StyleSheet.create({
   text: {
